857ec3141bdcbe69a921ca49b01f2d9eaec253a6
furkanyg <furselyalcin27@gmail.com>
2024-03-09 21:50:38 +0300

    come on

---
+++ app.js
@@ -0,0 +1,12 @@
+
+let inputName = document.getElementById('input-text-name');
+let inputPassword = document.getElementById('input-text-name');
+let inputButton = document.getElementById('sign-in');
+
+window.onload = () =>{
+
+};
+function Clicked() {
+    let inputName = document.getElementById('input-text-name');
+    console.log(inputName.value);
+};
